e5f1b5390a513587d3f47fd9e6ee01e7a6b3cb64
Alexander Torstling <alexander@torstling.com>
2021-03-21 15:51:16 +0100

    more stabs in the dark

--- macos/fakechroot.rb
+++ macos/fakechroot.rb
@@ -10,17 +10,19 @@ class Fakechroot < Formula
 
   def install
     inreplace 'src/__opendir2.c', '__FBSDID', '//__FBSDID'
-    inreplace 'src/__opendir2.c', 'dd_td', '__dd_td'
-    inreplace 'src/__opendir2.c', 'dd_buf', '__dd_buf'
-    inreplace 'src/__opendir2.c', 'dd_fd', '__dd_fd'
-    inreplace 'src/__opendir2.c', 'dd_loc', '__dd_loc'
-    inreplace 'src/__opendir2.c', 'dd_size', '__dd_size'
-    inreplace 'src/__opendir2.c', 'dd_len', '__dd_len'
-    inreplace 'src/__opendir2.c', 'dd_seek', '__dd_seek'
-    inreplace 'src/__opendir2.c', 'dd_rewind', '__dd_rewind'
-    inreplace 'src/__opendir2.c', 'dd_flags', '__dd_flags'
-    inreplace 'src/__opendir2.c', 'dirp->__dd_lock = NULL;',
-                'pthread_mutex_init(dirp->__dd_lock, NULL);'
+    #inreplace 'src/__opendir2.c', 'dd_td', '__dd_td'
+    #inreplace 'src/__opendir2.c', 'dd_buf', '__dd_buf'
+    #inreplace 'src/__opendir2.c', 'dd_fd', '__dd_fd'
+    #inreplace 'src/__opendir2.c', 'dd_loc', '__dd_loc'
+    #inreplace 'src/__opendir2.c', 'dd_size', '__dd_size'
+    #inreplace 'src/__opendir2.c', 'dd_len', '__dd_len'
+    #inreplace 'src/__opendir2.c', 'dd_seek', '__dd_seek'
+    #inreplace 'src/__opendir2.c', 'dd_rewind', '__dd_rewind'
+    #inreplace 'src/__opendir2.c', 'dd_flags', '__dd_flags'
+    #inreplace 'src/__opendir2.c', 'dirp->__dd_lock = NULL;',
+    #            'pthread_mutex_init(dirp->__dd_lock, NULL);'
+    inreplace 'src/__opendir2.c', 'dirp->dd_lock = NULL;',
+                'pthread_mutex_init(dirp->dd_lock, NULL);'
     system "./configure", "--disable-silent-rules", "--prefix=#{prefix}"
     system "make -j 1"
     system "make install"
